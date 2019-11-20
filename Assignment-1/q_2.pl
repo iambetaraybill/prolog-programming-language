69ab116e6f7ed7e03d640ff0461063182f5aeaf9
Arpan <arpan98sarkar@gmail.com>
2019-11-20 17:42:25 +0530

    Heading given changed file name

--- Assignment-1/q_2.pl
+++ Assignment-1/q_2.pl
@@ -1,4 +1,8 @@
+%%%%%%%%%%%% Maximum of Two Elements %%%%%%%%%%%%%%%
+
 max(P,Q) :- P>Q, 
+    % X is P,
+    % format(" $~3f ", [X]),
     write('Larger number is '),
     write(P).
 max(P,Q) :- P<Q,
