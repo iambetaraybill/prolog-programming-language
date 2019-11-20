b47dc2c37ce8bd2499f8302a71f4f3773ab3f171
Arpan <arpan98sarkar@gmail.com>
2019-11-20 13:51:50 +0530

    Added semicolon to the string in write commands.

--- Assignment-1/maxOfTwoElements.pl
+++ Assignment-1/maxOfTwoElements.pl
@@ -1,8 +1,8 @@
 max(P,Q) :- P>Q, 
-    write(Larger number is),
+    write('Larger number is '),
     write(P).
 max(P,Q) :- P<Q,
-    write(Larger number is ),
+    write('Larger number is '),
     write(Q).
 max(P,Q) :- P=Q, 
-    write(Both are equal).
+    write('Both are equal').
